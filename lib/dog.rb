ae39d0dfb04e7e1458c3fd96ae25d086127534f4
Will <swilson.carlson@hotmail.com>
2022-08-09 09:27:49 -0400

    commit tree 5d5745b612fb37159016505effcede9ee8cd8c24

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
 
+    def name=(dog_name) #setting
+        @this_dogs_name = dog_name
+    end
+
+    def name #getter
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
